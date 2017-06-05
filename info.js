76c21bb01078bbb87679f7b82a76e584288439b8
John Shields <johnshields2012@gmail.com>
2017-06-05 21:21:52 +0100

    Add Info.js

---
+++ info.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Info {
+	constructor() {
+		this.classes = {};
+	}
+
+	loadClasses() {
+		let classes;
+		try {
+			classes = fs.readdirSync('./classes');
+		} catch (e) {}
+		if (classes) {
+			for (let i = 0, len = classes.length; i < len; i++) {
+				let fileName = classes[i];
+				if (!fileName.endsWith('.js')) continue;
+				let characterClass = require('./classes/' + fileName);
+				this.classes[characterClass.id] = characterClass;
+			}
+			for (let i in this.classes) {
+				let characterClass = this.modes[i];
+				if (characterClass.aliases) {
+					for (let i = 0, len = characterClass.aliases.length; i < len; i++) {
+						let alias = Tools.toId(characterClass.aliases[i]);
+						if (alias in this.classes) throw new Error(characterClass.name + " alias '" + alias + "' is already a class.");
+						this.classes[alias] = characterClass;
+						characterClass.aliases[i] = alias;
+					}
+				}
+			}
+		}
+	}
+}
+
+module.exports = new Info();
