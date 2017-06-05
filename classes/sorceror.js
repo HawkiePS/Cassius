eb51f763b7460e19bc9dd2347476de37b4deb633
John Shields <johnshields2012@gmail.com>
2017-06-05 23:05:42 +0100

    Update sorceror.js

--- classes/sorceror.js
+++ classes/sorceror.js
@@ -1,6 +1,34 @@
 'use strict';
 
-class Sorceror {}
+const name = "Sorceror";
+
+class Sorceror {
+	constructor() {
+		this.statProgression = {
+			health: 3,
+			vitality: 0.175,
+			attack: 0.1,
+			strength: 0.1,
+			defence: 0.5,
+			resilience: 0.25,
+			magic: 3,
+			intelligence: 1.25,
+			mdefence: 2,
+			wisdom: 0.75,
+			endurance: 0.75,
+			recovery: 0.25,
+			speed: 0,
+			agility: 0.1,
+			critical: 0.1,
+			evasion: 0.1
+		};
+	}
+	getStatGains(stat, level) {
+		let levelUps = parseInt(level);
+		levelUps--;
+		return this.statProgression[stat] * levelUps;
+	}
+}
 
 
 module.exports = new Sorceror();
